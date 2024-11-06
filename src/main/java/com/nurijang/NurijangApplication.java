df10d2cff50cee216019b5a2ed469bdd6f6ec33e
socury <socury1104@gmail.com>
2024-11-06 12:20:28 +0900

    first commit

---
+++ src/main/java/com/nurijang/NurijangApplication.java
@@ -0,0 +1,13 @@
+package com.nurijang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurijangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurijangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nurijang/NurijangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurijang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurijangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
