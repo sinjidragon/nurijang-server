f5b7d9db65d4549ccaea1dcd7d276ae3951cb584
socury <socury1104@gmail.com>
2024-11-07 11:30:16 +0900

    docs: swagger docs

--- src/main/java/com/nurijang/controller/FacilityController.java
+++ src/main/java/com/nurijang/controller/FacilityController.java
@@ -19,13 +19,29 @@ public class FacilityController {
     private final FacilityRepository facilityRepository;
     private final FacilityService facilityService;
 
-    @Operation(summary = "시설 상세정보", description = "시설 id를 파라미터에 입력")
+    @Operation(summary = "시설 상세정보", description = "시설 id를 파라미터에 입력" +
+            "       \n" +
+            "\n" +
+            "      ctprvnCd 시도코드\n" +
+            "      ctprvnNm 시도명\n" +
+            "      signguCd 시군구코드\n" +
+            "      signguNm 시군구명\n" +
+            "      fcltyNm 시설명\n" +
+            "      rprsntvTelNo 대표자전화번호\n" +
+            "      zipNo 우편번호\n" +
+            "      fcltyAddr 시설주소\n" +
+            "      fcltyDetailAddr 시설상세주소\n" +
+            "      mainItemCd 주요종목코드\n" +
+            "      mainItemNm 주요종목명\n" +
+            "      fcltyCrdntLo 시설좌표경도\n" +
+            "      fcltyCrdntLa 시설좌표위도\n" +
+            "      courseFlagCd 강좌구분코드")
     @GetMapping("/{id}")
     public FacilityEntity getFacility(@RequestParam int id) {
         return facilityRepository.findById(id);
     }
 
-    @Operation(summary = "주변 시설 정보", description = "위도 경도 입력")
+    @Operation(summary = "주변 시설 정보", description = "위도 경도 순으로 입력")
     @PostMapping("/facilities")
     public List<GetFacilitiesResponse> getFacilities(@RequestBody GetFacilitiesRequest getFacilitiesRequest) {
         return facilityService.getFacilities(getFacilitiesRequest);
