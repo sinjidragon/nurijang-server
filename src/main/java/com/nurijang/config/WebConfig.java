e68eb35cafb3ea1ff937091f0166d17b1cf334d8
socury <socury1104@gmail.com>
2024-11-28 12:06:39 +0900

    refactor: cors를 모든 도메인에 허용

--- src/main/java/com/nurijang/config/WebConfig.java
+++ src/main/java/com/nurijang/config/WebConfig.java
@@ -1,18 +1,13 @@
 package com.nurijang.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${cors.allowedOrigins}")
-    private String allowedOrigins;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
