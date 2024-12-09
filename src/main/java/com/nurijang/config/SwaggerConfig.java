4dc23c949cf1dfb358b602c113ce6dba20143ed2
socury <socury1104@gmail.com>
2024-12-10 08:40:51 +0900

    fix: swagger server url

--- src/main/java/com/nurijang/config/SwaggerConfig.java
+++ src/main/java/com/nurijang/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +16,9 @@ public class SwaggerConfig {
                 .info(
                         new Info().title("nurijang API")
                                 .version("1.0.1")
-                );
+                )
+                .servers(Collections.singletonList(
+                        new Server().url("https://api.nurijang.p-e.kr").description("Production server")
+                ));
     }
 }
